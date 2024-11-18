41a065e525134f05644cff07e327b07fee4bf8d6
alirzamahmoodi <137698140+alirzamahmoodi@users.noreply.github.com>
2024-11-18 16:32:21 +0330

    aligned NLayerDiscriminator and MultiScaleDiscriminator with Pix2pixHD

--- models/networks.py
+++ models/networks.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -541,9 +542,9 @@ class UnetSkipConnectionBlock(nn.Module):
 
 
 class NLayerDiscriminator(nn.Module):
-    """Defines a PatchGAN discriminator"""
+    """Defines a PatchGAN discriminator with intermediate feature extraction option."""
 
-    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, getIntermFeat=False):
         """Construct a PatchGAN discriminator
 
         Parameters:
@@ -551,41 +552,54 @@ class NLayerDiscriminator(nn.Module):
             ndf (int)       -- the number of filters in the last conv layer
             n_layers (int)  -- the number of conv layers in the discriminator
             norm_layer      -- normalization layer
+            getIntermFeat   -- whether to return intermediate features
         """
         super(NLayerDiscriminator, self).__init__()
-        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
+        self.getIntermFeat = getIntermFeat
+        self.n_layers = n_layers
 
         kw = 4
-        padw = 1
-        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
-        nf_mult = 1
-        nf_mult_prev = 1
-        for n in range(1, n_layers):  # gradually increase the number of filters
-            nf_mult_prev = nf_mult
-            nf_mult = min(2 ** n, 8)
-            sequence += [
-                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
-                norm_layer(ndf * nf_mult),
+        padw = int(np.ceil((kw - 1.0) / 2))
+        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]]
+
+        nf = ndf
+        for n in range(1, n_layers):
+            nf_prev = nf
+            nf = min(nf * 2, 512)
+            sequence += [[
+                nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=2, padding=padw),
+                norm_layer(nf),
                 nn.LeakyReLU(0.2, True)
-            ]
+            ]]
 
-        nf_mult_prev = nf_mult
-        nf_mult = min(2 ** n_layers, 8)
-        sequence += [
-            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
-            norm_layer(ndf * nf_mult),
+        nf_prev = nf
+        nf = min(nf * 2, 512)
+        sequence += [[
+            nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=1, padding=padw),
+            norm_layer(nf),
             nn.LeakyReLU(0.2, True)
-        ]
+        ]]
 
-        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
-        self.model = nn.Sequential(*sequence)
+        sequence += [[nn.Conv2d(nf, 1, kernel_size=kw, stride=1, padding=padw)]]
+
+        if getIntermFeat:
+            for n in range(len(sequence)):
+                setattr(self, 'model' + str(n), nn.Sequential(*sequence[n]))
+        else:
+            sequence_stream = []
+            for n in range(len(sequence)):
+                sequence_stream += sequence[n]
+            self.model = nn.Sequential(*sequence_stream)
 
     def forward(self, input):
-        """Standard forward."""
-        return self.model(input)
+        if self.getIntermFeat:
+            res = [input]
+            for n in range(self.n_layers + 2):
+                model = getattr(self, 'model' + str(n))
+                res.append(model(res[-1]))
+            return res[1:]
+        else:
+            return self.model(input)
 
 
 class PixelDiscriminator(nn.Module):
@@ -623,30 +637,53 @@ class PixelDiscriminator(nn.Module):
 class MultiScaleDiscriminator(nn.Module):
     """Defines a multi-scale PatchGAN discriminator"""
 
-    def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d, num_discriminators=3):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, num_D=3, getIntermFeat=False):
         """Construct a MultiScale PatchGAN discriminator
 
         Parameters:
             input_nc (int)      -- the number of channels in input images
             ndf (int)           -- the number of filters in the first conv layer
             norm_layer          -- normalization layer
-            num_discriminators  -- number of discriminators working at different scales
+            num_D (int)         -- number of discriminators working at different scales
+            getIntermFeat       -- whether to return intermediate features
         """
         super(MultiScaleDiscriminator, self).__init__()
-        self.num_discriminators = num_discriminators
-        self.discriminators = nn.ModuleList()
+        self.num_D = num_D
+        self.n_layers = n_layers
+        self.getIntermFeat = getIntermFeat
 
-        for _ in range(num_discriminators):
-            self.discriminators.append(NLayerDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer))
+        for i in range(num_D):
+            netD = NLayerDiscriminator(input_nc, ndf, n_layers, norm_layer, getIntermFeat)
+            if getIntermFeat:
+                for j in range(n_layers + 2):
+                    setattr(self, 'scale' + str(i) + '_layer' + str(j), getattr(netD, 'model' + str(j)))
+            else:
+                setattr(self, 'layer' + str(i), netD.model)
+
+        self.downsample = nn.AvgPool2d(3, stride=2, padding=[1, 1], count_include_pad=False)
+
+    def singleD_forward(self, model, input):
+        if self.getIntermFeat:
+            result = [input]
+            for i in range(len(model)):
+                result.append(model[i](result[-1]))
+            return result[1:]
+        else:
+            return [model(input)]
 
     def forward(self, input):
-        """Standard forward for multi-scale discriminator."""
-        outputs = []
-        for discriminator in self.discriminators:
-            outputs.append(discriminator(input))
-            input = F.avg_pool2d(input, kernel_size=2)  # Downscale input for the next discriminator
-
-        return outputs
+        num_D = self.num_D
+        result = []
+        input_downsampled = input
+        for i in range(num_D):
+            if self.getIntermFeat:
+                model = [getattr(self, 'scale' + str(num_D - 1 - i) + '_layer' + str(j)) for j in range(self.n_layers + 2)]
+            else:
+                model = getattr(self, 'layer' + str(num_D - 1 - i))
+            result.append(self.singleD_forward(model, input_downsampled))
+            if i != (num_D - 1):
+                input_downsampled = self.downsample(input_downsampled)
+        return result
 
 ###################################################################################################################
 ##################################            ResUnetPlusPlus                ######################################
